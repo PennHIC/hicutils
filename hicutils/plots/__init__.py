18f3fcc263bc2b81a619035d840e63be504bacd8
Aaron Rosenfeld <aaron.m.rosenfeld@gmail.com>
2023-01-31 13:45:53 -0500

    cdr3 length analysis

--- hicutils/plots/__init__.py
+++ hicutils/plots/__init__.py
@@ -11,7 +11,8 @@ from .gene_usage import plot_gene_usage  # noqa: F401
 from .cdr3_analysis import (  # noqa: F401
     plot_cdr3_aa_usage,
     plot_cdr3_logo,
-    plot_cdr3_spectratype
+    plot_cdr3_spectratype,
+    plot_cdr3_distribution
 )
 from .shm import (  # noqa: F401
     plot_shm_distribution,
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as fh:
 
 setup(
     name='hicutils',
-    version='0.0.1',
+    version='0.0.3',
     author='Aaron M. Rosenfeld',
     author_email='aaron.rosenfeld@pennmedicine.upenn.edu',
     url='https://github.com/PennHIC/hicutils',
